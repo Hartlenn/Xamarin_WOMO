50fde376ee5ac0dea02e05463f7ca033cc8cc4c0
Florian <Florian@LAPTOP>
2016-11-16 11:43:54 +0100

    menu designed, functions added

--- Xamarin_WoMo_project/WoMo/WoMo/UI/MainPage.xaml.cs
+++ Xamarin_WoMo_project/WoMo/WoMo/UI/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WoMo.UI;
 using Xamarin.Forms;
 
 namespace WoMo
@@ -12,6 +13,26 @@ namespace WoMo
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void OnStellplatzClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Stellplatz_Uebersicht());
+        }
+
+        async void OnChecklistClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Checklist());
+        }
+
+        async void OnTagebuchClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Tagebuch());
+        }
+
+        async void OnXMLVerwaltungClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
